a3c7bdc74f58d482d0fbeeebd907596132e5a801
Greg Wiley <gwiley@cj.com>
2018-12-02 13:13:51 -0800

    checkpoint

--- test/Spec/CRDT.hs
+++ test/Spec/CRDT.hs
@@ -15,50 +15,50 @@ main = defaultMain tests
 tests :: TestTree
 tests = testGroup "CRDT" [
 
-        testCase "initial insert" $
-            insert mempty 0 'a' @?= (V.singleton (Cell (1 % 2) 'a'), Cell (1 % 2) 'a'),
+    testCase "initial insert" $
+        insert mempty 0 'a' @?= (V.singleton (Cell (1 % 2) 'a'), Cell (1 % 2) 'a'),
 
-        testCase "insert returns inserted" $
-            let c = 'b'
-                actual = snd (insert (insert' mempty 0 'z') 0 c)
-            in value actual @?= c,
+    testCase "insert returns inserted" $
+        let c = 'b'
+            actual = snd (insert (insert' mempty 0 'z') 0 c)
+        in value actual @?= c,
 
-        testCase "emit as text" $
-            let buf = V.fromList [Cell (1 % 4) 'a', Cell (1 % 2) 'b', Cell (3 % 4) 'c']
-            in emit buf @?= "abc",
+    testCase "emit as text" $
+        let buf = V.fromList [Cell (1 % 4) 'a', Cell (1 % 2) 'b', Cell (3 % 4) 'c']
+        in emit buf @?= "abc",
 
-        testCase "insert after all" $
-            let orig = insert' mempty 0 'a'
-                actual = insert' orig 1 'b'
-            in emit actual @?= "ab",
+    testCase "insert after all" $
+        let orig = insert' mempty 0 'a'
+            actual = insert' orig 1 'b'
+        in emit actual @?= "ab",
 
-        testCase "insert way after" $
-            let orig = insert' mempty 0 'a'
-                actual = insert' orig 100 'b'
-            in emit actual @?= "ab",
+    testCase "insert way after" $
+        let orig = insert' mempty 0 'a'
+            actual = insert' orig 100 'b'
+        in emit actual @?= "ab",
 
-        testCase "insert before all" $
-            let orig = insert' mempty 0 'a'
-                actual = insert' orig 0 'b'
-            in emit actual @?= "ba",
+    testCase "insert before all" $
+        let orig = insert' mempty 0 'a'
+            actual = insert' orig 0 'b'
+        in emit actual @?= "ba",
 
-        testCase "insert between" $
-            let orig = insert' (insert' mempty 0 'a') 1 'b'
-                actual = insert' orig 1 'c'
-            in emit actual @?= "acb",
+    testCase "insert between" $
+        let orig = insert' (insert' mempty 0 'a') 1 'b'
+            actual = insert' orig 1 'c'
+        in emit actual @?= "acb",
 
-        testCase "unique identifiers" $
-            let x = insert' (insert' mempty 1 'a') 1 'a'
-                y = insert' (insert' x 0 'a') 0 'a'
-                actual = insert' (insert' y 2 'a') 1 'a'
-                unique = S.fromList . V.toList . fmap uid $ actual
-            in Prelude.length unique @?= 6,
-
-        testCase "increasing identifiers" $
+    testCase "unique identifiers" $
         let x = insert' (insert' mempty 1 'a') 1 'a'
             y = insert' (insert' x 0 'a') 0 'a'
             actual = insert' (insert' y 2 'a') 1 'a'
-        in snd (Prelude.foldl ascending (0, True) (fmap uid actual)) @?= True
+            unique = S.fromList . V.toList . fmap uid $ actual
+        in Prelude.length unique @?= 6,
+
+    testCase "increasing identifiers" $
+    let x = insert' (insert' mempty 1 'a') 1 'a'
+        y = insert' (insert' x 0 'a') 0 'a'
+        actual = insert' (insert' y 2 'a') 1 'a'
+    in snd (Prelude.foldl ascending (0, True) (fmap uid actual)) @?= True
     ]
 
 ascending :: (Ord a) => (a, Bool) -> a -> (a, Bool)
